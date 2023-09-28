37c9e279e407a3fd1f5bc054569775b24c417a93
Arielle Gironza <ariellegironza@Arielles-MacBook-Air.local>
2023-09-28 00:16:35 -0400

    loaders.js

--- src/loaders.js
+++ src/loaders.js
@@ -2,9 +2,29 @@
 const URL = "https://will-friend-backend.onrender.com"
 
 // willIndexLoader - Get all WILLS for Will Index Route
-
-// assetIndexLoader - Get all ASSETS for Asset Index Route
+export const willIndexLoader = async () => {
+    const response = await fetch(URL + "/will/")
+    const wills = await response.json()
+    return wills
+}
 
 // willShowLoader - Get all WILLS for Will Show Route
+export const willShowLoader = async ({params}) => {
+    const response = await fetch(URL + `/will/${params.id}/`)
+    const will = await response.json()
+    return will
+}
+
+// assetIndexLoader - Get all ASSETS for Asset Index Route
+export const assetIndexLoader = async () => {
+    const response = await fetch(URL + "/asset/")
+    const assets = await response.json()
+    return assets
+}
 
 // assetShowLoader - Get all ASSETS for Asset Show Route
+export const assetShowLoader = async ({params}) => {
+    const response = await fetch(URL + `/asset/${params.id}`)
+    const asset = await response.json()
+    return asset
+}
