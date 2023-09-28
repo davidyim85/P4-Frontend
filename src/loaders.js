4bf287aef5d39a95a415ecfa2542a8729f753e50
Arielle Gironza <ariellegironza@Arielles-MacBook-Air.local>
2023-09-27 23:55:36 -0400

    drafting loaders

---
+++ src/loaders.js
@@ -0,0 +1,10 @@
+// DEPLOYED API BASE URL
+const URL = "https://will-friend-backend.onrender.com"
+
+// willIndexLoader - Get all WILLS for Will Index Route
+
+// assetIndexLoader - Get all ASSETS for Asset Index Route
+
+// willShowLoader - Get all WILLS for Will Show Route
+
+// assetShowLoader - Get all ASSETS for Asset Show Route
--- src/router.js
+++ src/router.js
@@ -1,10 +1,19 @@
 import {createBrowserRouter, createRoutesFromElements, Route, Routes} from "react-router-dom"
 import App from "./App"
+import Index from "./pages/Will/Index"
+import Index from "./pages/Asset/Index"
+import Show from "./pages/Will/Show"
+import Show from "./pages/Asset/Show"
+
 
 const router = createBrowserRouter(createRoutesFromElements(
     <>
         <Route path="/" element={<App/>}>
-
+            <Route path="" element={<Index/>}/>
+            <Route path="will/:id" element={<Show/>}/>
+            <Route path="create"/>
+            <Route path="update/:id"/>
+            <Route path="delete/:id"/>
         </Route>
     </>
 ))
